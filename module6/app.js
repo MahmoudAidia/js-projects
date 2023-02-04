05398f66ee7f7c28662ff4115f7373fc7cebb52f
Mahmoud Mostafa Aidia <116117523+MahmoudAidia@users.noreply.github.com>
2023-02-04 07:01:30 +0200

    Initital commit for module 6

---
+++ module6/app.js
@@ -0,0 +1,17 @@
+const startGameBtn = document.getElementById("start-game-btn");
+
+// const start = function startGame() {
+//   console.log("Game is starting ...");
+// };
+
+// let person = {
+//   curse: function greet() {
+//     console.log("Hello there !");
+//   },
+// };
+// Note that here we called the function (acutually the method) with the name of the key that it is stored in it
+// person.curse();
+
+startGameBtn.addEventListener("click", function () {
+  console.log("Game is starting...");
+});
